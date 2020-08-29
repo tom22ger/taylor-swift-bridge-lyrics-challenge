be71eaf99e9d384512a2c0353fc006e912252a15
tom22ger <thomasadrian@mac.com>
2020-08-29 01:09:48 -0400

    initial commit

---
+++ precache-manifest.b3c240c55ccdffbb840bd2a15a52a2b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895955a443d65f463e21c5d556108f38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21229c428bb9ad7c8640",
+    "url": "/static/css/main.32c41632.chunk.css"
+  },
+  {
+    "revision": "1b3f3348826baf752939",
+    "url": "/static/js/2.025acd41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.025acd41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21229c428bb9ad7c8640",
+    "url": "/static/js/main.f1636452.chunk.js"
+  },
+  {
+    "revision": "2066eacb9a0682c3d1c8",
+    "url": "/static/js/runtime-main.3cf453d2.js"
+  },
+  {
+    "revision": "0eff6caf303b673456f0b3753349c529",
+    "url": "/static/media/bridge.0eff6caf.svg"
+  },
+  {
+    "revision": "e4119d60d91f1b583db543b6c23e9858",
+    "url": "/static/media/doublenote.e4119d60.svg"
+  },
+  {
+    "revision": "9ba2252aa41355b1eed62416da78ff9e",
+    "url": "/static/media/note.9ba2252a.svg"
+  }
+]);
